43d088c91d485a7fbebf2641f1d18d992e9dd36b
Sandy CS <89575609+Shaa94@users.noreply.github.com>
2022-08-29 11:00:11 +0200

    Add getattributefile

---
+++ surfspot/surfspot/surfspotApp.swift
@@ -0,0 +1,17 @@
+//
+//  surfspotApp.swift
+//  surfspot
+//
+//  Created by Sandy CARDOSO SIBORRO on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct surfspotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
